a05bd22b17308f5d1c8360c056485ee856b44315
vinalbagaria <vinalbagaria@gmail.com>
2020-09-06 08:11:41 +0530

    Heroku collectstatic error

--- VirtualVidyalaya/settings.py
+++ VirtualVidyalaya/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'VirtualVidyalaya.urls'
